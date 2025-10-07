3ed6ac54bc81139efbbb45cd36e3d168aae8c0b1
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-10-07 19:46:05 -0300

    Add files via upload

---
+++ api-exercicios-alura/exercicios/src/Livros.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Livros {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o nome do livro para a busca: ");
+        String busca = scan.nextLine();
+
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + busca +"&key=AIzaSyCcpOzy5m135l81mvVub5bQtTkceLYZLpo";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
